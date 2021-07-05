148d73e5026669e99cc73a8eedd6fce76c34b9fc
trish <pmgriego@gmail.com>
2021-07-04 21:36:49 -0700

    initial set up for files

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
+# load_dotenv()
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI"
+        )
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI"
+        )
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.models.countdown_event import CountdownEvent
+
+    from .routes import countdown_events_bp
+    app.register_blueprint(countdown_events_bp)
+
+    return app
