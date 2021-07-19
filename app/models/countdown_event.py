b789cd7061deb4d9ceca99b24c742e2fd8c86d50
trish <pmgriego@gmail.com>
2021-07-19 12:22:11 -0700

    removing unncessary comments

--- app/models/countdown_event.py
+++ app/models/countdown_event.py
@@ -1,8 +1,5 @@
 from app import db
 
-# This is one proposed model for CountdownEvent
-# Says Python classes should be CamelCase
-# https://visualgit.readthedocs.io/en/latest/pages/naming_convention.html#classes
 class CountdownEvent(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
