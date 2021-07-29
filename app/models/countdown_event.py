37f4713e1dbe058cd4d0e43f3f2bfea88c4221dc
trish <pmgriego@gmail.com>
2021-07-28 19:40:44 -0700

    update model to try and fix timezone problems

--- app/models/countdown_event.py
+++ app/models/countdown_event.py
@@ -5,6 +5,6 @@ class CountdownEvent(db.Model):
     __tablename__ = "countdown_event"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
-    countdown_till_date = db.Column(db.DateTime)
-    creation_date = db.Column(db.DateTime, default=datetime.utcnow)
+    countdown_till_date = db.Column(db.DateTime(timezone=True))
+    creation_date = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"), nullable=True)
